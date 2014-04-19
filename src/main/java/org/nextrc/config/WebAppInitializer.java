35a7ae2a91b5c86f61d61c80aab67dfcb8763bc4
Marcin Ślósarz <m.slosarz@ahej.pl>
2014-04-19 11:48:58 +0200

    Basic project structure added

---
+++ src/main/java/org/nextrc/config/WebAppInitializer.java
@@ -0,0 +1,39 @@
+package org.nextrc.config;
+
+import javax.servlet.Filter;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { WebConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[] { AppConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		return new Filter[] { characterEncodingFilter };
+	}
+
+}
+
+@Configuration
+@ComponentScan("org.nextrtc")
+class AppConfig {
+
+}
---
+++ src/main/java/org/nextrc/config/WebConfig.java
@@ -0,0 +1,11 @@
+package org.nextrc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+}
